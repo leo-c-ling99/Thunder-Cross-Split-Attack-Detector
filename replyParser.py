e8a9e8df910195cb8c2e8877828c51f5bd7e45b7
Leo Ling <leoling99@gmail.com>
2019-09-16 20:51:53 -0500

    Fixed subreddit filtering to ignore source sub

--- replyParser.py
+++ replyParser.py
@@ -18,18 +18,23 @@ reply_string = """Hey Gamers, Kakyoin bot here. This is likely a Thunder Cross S
 ***
 ^Beep ^Boop, ^I ^am ^a ^bot"""
 link_pattern =  r'https://www.reddit.com/r/youfellforitfool/comments/cjlngm/you_fell_for_it_fool'
-subredditStr = 'all-YouFellForItFool'
+subredditStr = 'all'
 
 def search(linkPattern, subredditStr):
 	# Grab all the Recent Comments in every subreddit. This will return 100 of the newest comments on Reddit
 	for results in reddit.subreddit(subredditStr).comments():
 		global seen_comments_id # Import global comment queues
 
+		comment_id = results.id  # Get the Comment ID
+		# Checks the subreddit, filters are ignored by the comments method
+		if results.subreddit == 'YouFellForItFool':
+			seen_comments_id.put(comment_id)
+			break
+
 		body = results.body  # Grab the Comment
 		# Convert the comment to lowercase so we can search it no matter how it was written
 		body = body.lower()
-		comment_id = results.id  # Get the Comment ID
-
+		
 		# Check if we already replied to this comment, break if so
         # Otherwise, add to seen comments queue
 		if comment_id in seen_comments_id.queue:
